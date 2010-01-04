d847e8dc88b2feb81c41fdcc23552e8a4e419bbc
puneet goyal <puneet@puneet.(none)>
2010-01-04 11:05:06 +0530

    foo

--- manage.py
+++ manage.py
@@ -1,3 +1,57 @@
+Editing a setting changes only the text in this buffer.
+To apply your changes, use the Save or Set buttons.
+Saving a change normally works by editing your init file.
+For details, see Saving Customizations in the Emacs manual.
+
+
+ Operate on all settings in this buffer that are not marked HIDDEN:
+ Set for current session Save for future sessions
+ Undo edits Reset to saved Erase customizations   Exit
+
+Parent groups: Tools
+
+/- Ecb group: Emacs code browser.----------------------------------------\
+      State: visible group members are all at standard values.
+   
+Ecb Download : Settings for downloading and installing a new ECB from within ECB.
+
+Ecb Face Options : Settings for all faces used in ECB.
+
+Ecb Tree Buffer : General settings related to the tree-buffers of ECB.
+
+Ecb Layout : Settings for the screen-layout of the Emacs code browser.
+
+Ecb Directories : Settings for the directories-buffer in the Emacs code browser.
+
+Ecb Sources : Settings for the sources-buffers in the Emacs code browser.
+
+Ecb History : Settings for the history-buffer in the Emacs code browser.
+
+Ecb Version Control : Settings for the version-control support in the ECB.
+
+Ecb Methods : Settings for the methods-buffer in the Emacs code browser.
+
+Ecb Non Semantic : Settings for parsing and displaying non-semantic files.
+
+Ecb Jde Integration : Settings for the JDEE-integration in the Emacs code browser.
+
+Ecb Help : Settings for the ECB online help
+
+Ecb Eshell : Settings for eshell integration within the ECB.
+
+Ecb Cycle : Setting for cycling through misc ECB buffers.
+
+Ecb Winman Support : Settings for supporting several window-managers. More
+
+Ecb Analyse : Settings for the analyse-buffer in the Emacs code browser.
+
+Ecb Symboldef : Settings for the symbol-definition-buffer in the Emacs code browser.
+
+Ecb General : General settings for the Emacs code browser.
+
+Ecb Most Important : The most important settings of ECB you should know.
+
+\- Ecb group end -------------------------------------------------------/
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
