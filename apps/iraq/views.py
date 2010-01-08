f1b644e1c7eee4fa903f8d54e6b01acd20914ee6
puneet goyal <puneet@puneet.(none)>
2010-01-08 16:19:50 +0530

    removing view

--- apps/iraq/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.decorators.http import require_GET, require_POST
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from iraq.models import *
-
-
-def Dashboard(request):
-    return HttpResponse("hello")
-
