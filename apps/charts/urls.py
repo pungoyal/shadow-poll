921910893dc41f4be17c76ea914c591a105f3866
Diptanu Choudhury <admin.nitjece@gmail.com>
2010-01-12 16:21:52 +0530

    Adding the static document root to urls.py

--- apps/charts/urls.py
+++ apps/charts/urls.py
@@ -6,5 +6,8 @@ urlpatterns = patterns('charts',
                        (r'^map/$', 'views.show_stats_on_map'),
                        )
 
-
-
+urlpatterns += patterns('',
+                        (r'^static/(?P<path>.*)$', 
+                         'django.views.static.serve',
+                         {'document_root': 'static'})
+                         )
