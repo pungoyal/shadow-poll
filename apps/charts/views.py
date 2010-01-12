94750f92f79a614cd5153b3becb9259ae48bf898
Diptanu Choudhury <admin.nitjece@gmail.com>
2010-01-12 15:43:07 +0530

    Hooked the template to the view

--- apps/charts/views.py
+++ apps/charts/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpRequest, HttpResponse
-from charts.models import Responses
+from django.shortcuts import render_to_response
+from django.template import loader, Context
 import datetime
 
 def show_stats_on_map(request):
-    return HttpResponse("Hello World")
+    return render_to_response('stats_map.html')
 
 def current_status(self):
     now = datetime.datetime.now()
     html = "<html><body>Current time is %s.</body></html> " % now
     return HttpResponse(html)
     
-
