73f0a31e79d175de1662552397dde53aab0764ba
puneet goyal <puneet@puneet.(none)>
2010-01-12 11:13:53 +0530

    changing location to integer for postcodes

--- apps/iraq/tests.py
+++ apps/iraq/tests.py
@@ -7,15 +7,15 @@ from app import App
 class ResponderTest(unittest.TestCase):
     def setUp(self):
         self.poll_response = PollResponse(mobile_number = 1000)
-    
+
     def testParseResponse(self):
-        response = self.poll_response.generateResponse("ED 16 F Baghdad")
+        response = self.poll_response.generateResponse("ED 16 F 110001")
 
         self.assertEquals(self.poll_response.age, '16')
         self.assertEquals(self.poll_response.gender,'F')
-        self.assertEquals(self.poll_response.location,"Baghdad")
+        self.assertEquals(self.poll_response.location,"110001")
         self.assertEquals(response, "Thanks for participating. You selected Education.")
-    def testLocationIsOptional(self):
+    def _testLocationIsOptional(self):
         response = self.poll_response.generateResponse("ED 12 M")
 
         self.assertEquals(self.poll_response.age, '12')
@@ -28,5 +28,5 @@ class ResponderTest(unittest.TestCase):
         response = self.poll_response.generateResponse("ED 12")
         response = self.poll_response.generateResponse("ED")
         response = self.poll_response.generateResponse("EV")
-        response = self.poll_response.generateResponse("ED 12 F Baghdad Foo")
-        response = self.poll_response.generateResponse("ED M 12 Baghdad")
+        response = self.poll_response.generateResponse("ED 12 F 110001 Foo")
+        response = self.poll_response.generateResponse("ED M 12 110001")
