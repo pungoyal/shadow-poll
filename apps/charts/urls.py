af43085ef8fe6f1f32e338205549de57ab92e5cb
Diptanu Choudhury <admin.nitjece@gmail.com>
2010-01-14 18:13:58 +0530

    Send req to Charts to get the poll stats

--- apps/charts/urls.py
+++ apps/charts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('charts',
                        (r'^charts/$', 'views.current_status'),
                        (r'^map/$', 'views.show_stats_on_map'),
                        (r'^data/(?P<path>.*)$','views.data'),
+                       (r'^get_stat/$', 'views.get_stats')
                        )
 
 handler404 = 'charts.views.view_404'
--- apps/charts/views.py
+++ apps/charts/views.py
@@ -4,11 +4,30 @@ from django.shortcuts import render_to_response
 from django.template import loader, Context
 from charts.chart_data import ChartData
 from django.template.context import RequestContext
+from httplib import HTTPResponse
+import urllib2
 
 import datetime
 
 def show_stats_on_map(request):
     return render_to_response('stats_map.html')
+
+def get_stats(request):
+    if request.method == 'GET':
+        left = request.GET.get('left')
+        bottom = request.GET.get('bottom')
+        right = request.GET.get('right')
+        top = request.GET.get('top')
+        x = request.GET.get('x')
+        y = request.GET.get('y')
+        url = ("http://localhost/geoserver/wms/reflector?bbox=%s,%s,%s,%s&\
+format=jpeg&info_format=text/plain&request=GetFeatureInfo&layers=GADM:IRQ_adm2&\
+query_layers=GADM:IRQ_adm2&width=550&height=250&x=%s&y=%s" % (left, bottom, right, top, x, y))
+        request = urllib2.urlopen(url)
+        response_dict = request.read()
+        print response_dict
+        return HttpResponse("OK")
+        
 
 def current_status(self):
     now = datetime.datetime.now()
