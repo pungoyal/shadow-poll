b7b47835e9123892f250a76954bc5a06efb1f53a
puneet goyal <puneet@puneet.(none)>
2010-01-15 12:11:09 +0530

    misc

--- apps/iraq/subtests/poll_response.py
+++ apps/iraq/subtests/poll_response.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 import rapidsms
 from iraq.models import *
-from rapidsms.tests.scripted import TestScript
-from iraq.app import App
 
 class PollResponseTest(TestCase):
     fixtures = ['poll_responses']
@@ -31,7 +29,7 @@ class PollResponseTest(TestCase):
         self.assertEquals(self.poll_response.age, '16')
         self.assertEquals(self.poll_response.gender,'F')
         self.assertEquals(self.poll_response.location,"110001")
-        self.assertEquals(response, "Thank you for voting. You selected Education as your number one issue.")        
+        self.assertEquals(response, "Thank you for voting. You selected Education as your number one issue.")
 
     def test_incorrect_response_message_on_bad_parsing(self):
         error_message = "Sorry, we did not understand your response. Please re-send as - issue age gender area"
