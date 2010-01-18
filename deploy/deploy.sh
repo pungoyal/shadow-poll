5da1f2b2ab45a5cf3b661759173265d92ce8c408
puneet goyal <puneet@puneet.(none)>
2010-01-18 18:17:18 +0530

    deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 
 python /var/lib/buildbot/slave/shadow-poll/build/manage.py syncdb --noinput
-rm /var/www/shadowpoll/
+rm -rf /var/www/shadowpoll/*
 cp -r /var/lib/buildbot/slave/shadow-poll/build/* /var/www/shadowpoll
 
