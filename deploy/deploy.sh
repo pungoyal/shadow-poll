ee576e4b7974f2b501bef2858da30f7c93d494fa
puneet goyal <puneet@puneet.(none)>
2010-01-18 21:32:32 +0530

    fixing deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,4 +4,4 @@ kill -9 $(cat /tmp/shadowpoll.pid)
 python /var/lib/buildbot/slave/shadow-poll/build/manage.py syncdb --noinput
 rm -rf /var/www/shadowpoll/*
 cp -r /var/lib/buildbot/slave/shadow-poll/build/* /var/www/shadowpoll
-python /var/www/shadow-poll/manage.py runfcgi host=127.0.0.1 port=8801 pidfile=/tmp/shadowpoll.pid
+python /var/www/shadowpoll/manage.py runfcgi host=127.0.0.1 port=8801 pidfile=/tmp/shadowpoll.pid
