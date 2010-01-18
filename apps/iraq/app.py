8682e2eb65feb17d440ffdce8eda91e8d609a99b
puneet goyal <puneet@puneet.(none)>
2010-01-18 11:32:06 +0530

    multiple responses from same number

--- apps/iraq/app.py
+++ apps/iraq/app.py
@@ -5,14 +5,7 @@ from datetime import datetime
 
 class App (rapidsms.app.App):
     def handle (self, message):
-        number = message.connection.identity
-        querySet = PollResponse.objects.filter(mobile_number=number)[:1]
-
-        if (querySet.count() == 0):
-            poll_response = PollResponse(mobile_number = number)
-        else:
-            poll_response = querySet[0]
-        
+        poll_response = PollResponse(mobile_number = message.connection.identity)
         response_text = poll_response.generate_response(message.text)
         message.respond(response_text)
         return True
