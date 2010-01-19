2580dd031b5458527531f9ff183701a4cd47ae24
puneet goyal <puneet@puneet.(none)>
2010-01-19 17:01:06 +0530

    copying the file instead of symlinking

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,7 @@
 
 kill -9 $(cat /tmp/shadowpoll.pid)
 rm -rf /var/www/shadowpoll/*
-ln -s /var/lib/buildbot/slave/shadow-poll/build/rapidsms.ini.ci /var/lib/buildbot/slave/shadow-poll/build/rapidsms.ini
+cp /var/lib/buildbot/slave/shadow-poll/build/rapidsms.ini.ci /var/lib/buildbot/slave/shadow-poll/build/rapidsms.ini
 python /var/lib/buildbot/slave/shadow-poll/build/manage.py syncdb --noinput
 python /var/lib/buildbot/slave/shadow-poll/build/manage.py loaddata poll_responses.json
 cp -r /var/lib/buildbot/slave/shadow-poll/build/* /var/www/shadowpoll
