de8daeb390662dfe7d56712918d3bd8890e7fd08
puneet goyal <puneet@puneet.(none)>
2010-01-19 15:39:30 +0530

    bifurcating rapidsms settings file for dev and ci

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+!/bin/sh
 
 kill -9 $(cat /tmp/shadowpoll.pid)
 python /var/lib/buildbot/slave/shadow-poll/build/manage.py syncdb --noinput
 rm -rf /var/www/shadowpoll/*
 cp -r /var/lib/buildbot/slave/shadow-poll/build/* /var/www/shadowpoll
-python /var/www/shadowpoll/manage.py runfcgi -v 2 host=127.0.0.1 port=8801 debug=1 pidfile=/tmp/shadowpoll.pid outlog=/var/log/shadowpoll/fcgi.log errlog=/var/log/shadowpoll/fcgi-error.log
+ln -s /var/www/shadowpoll/rapidsms.ini.ci /var/www/shadowpoll/rapidsms.ini
+python /var/www/shadowpoll/manage.py runfcgi -v 2 host=127.0.0.1 port=8801 pidfile=/tmp/shadowpoll.pid outlog=/var/log/shadowpoll/fcgi.log errlog=/
