6485a160a179531789e08bc6999c9127afd8b36c
puneet goyal <puneet@puneet.(none)>
2010-01-19 18:46:23 +0530

    reverting routing process as of now

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,4 +10,3 @@ python /var/lib/buildbot/slave/shadow-poll/build/manage.py loaddata poll_respons
 
 cp -r /var/lib/buildbot/slave/shadow-poll/build/* /var/www/shadowpoll
 python /var/www/shadowpoll/manage.py runfcgi -v 2 host=127.0.0.1 port=8801 pidfile=/tmp/shadowpoll.pid outlog=/var/log/shadowpoll/fcgi.log errlog=/var/log/shadowpoll/fcgi-errors.log
-python /var/www/shadowpoll/manage.py route &
