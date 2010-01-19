a82165980a8aa0687eb74bc58a8062f008dff04b
puneet goyal <puneet@puneet.(none)>
2010-01-19 16:47:24 +0530

    deleting old build before deploying a newer one

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 kill -9 $(cat /tmp/shadowpoll.pid)
+rm -rf /var/www/shadowpoll/*
 ln -s /var/lib/buildbot/slave/shadow-poll/build/rapidsms.ini.ci /var/lib/buildbot/slave/shadow-poll/build/rapidsms.ini
 python /var/lib/buildbot/slave/shadow-poll/build/manage.py syncdb --noinput
 python /var/lib/buildbot/slave/shadow-poll/build/manage.py loaddata poll_responses.json
-rm -rf /var/www/shadowpoll/*
 cp -r /var/lib/buildbot/slave/shadow-poll/build/* /var/www/shadowpoll
 python /var/www/shadowpoll/manage.py runfcgi -v 2 host=127.0.0.1 port=8801 pidfile=/tmp/shadowpoll.pid outlog=/var/log/shadowpoll/fcgi.log errlog=/
