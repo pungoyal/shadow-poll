a3bc42c109a13a1baede64f1ffe658fb51982765
puneet goyal <puneet@puneet.(none)>
2010-01-19 17:07:07 +0530

    bad log file name

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,4 +6,4 @@ cp /var/lib/buildbot/slave/shadow-poll/build/rapidsms.ini.ci /var/lib/buildbot/s
 python /var/lib/buildbot/slave/shadow-poll/build/manage.py syncdb --noinput
 python /var/lib/buildbot/slave/shadow-poll/build/manage.py loaddata poll_responses.json
 cp -r /var/lib/buildbot/slave/shadow-poll/build/* /var/www/shadowpoll
-python /var/www/shadowpoll/manage.py runfcgi -v 2 host=127.0.0.1 port=8801 pidfile=/tmp/shadowpoll.pid outlog=/var/log/shadowpoll/fcgi.log errlog=/
+python /var/www/shadowpoll/manage.py runfcgi -v 2 host=127.0.0.1 port=8801 pidfile=/tmp/shadowpoll.pid outlog=/var/log/shadowpoll/fcgi.log errlog=/var/log/shadowpoll/fcgi-errors.log
