a8e4150381772f263e6c49efed25f6d2a5101aab
Diptanu Choudhury <diptanuc@Diptanu-Choudhurys-MacBook.local>
2010-01-19 16:50:34 +0530

    Added some more data for demo

--- apps/charts/postcode_name_map.py
+++ apps/charts/postcode_name_map.py
@@ -1,14 +1,14 @@
 __postcode_name_map = {'Ar Ramadi' :'31001', 'Al Fallujah' : '31002',
                        'Al Qa\'im':'31003' , 'Al Haditha' : '31004',
-                       'Enna': '31005',  'Rawah' :'31006' ,
+                       'Samarra': '31005',  'Rawah' :'31006' ,
                        'Hit':'31007',  'Al Habanyah':'31008',
-                       'Al Haqaniyah' :'31009', 'Al Khaldiya' : '31010',
+                       'Al Haqaniyah' :'31009', 'Balad Ruz' : '31010',
                        'Ar Rutbah':'31011' , 'Anah' : '31012', 
                        'Al Karmah': '31013', 'Al Baghdadi' : '31014',
-                       'Al Waleed': '31015' ,'31016' :'Al-Nukhaib' ,
-                       'Al Ubaidi':'31017','Kabisa' : '31018',
-                       'Tarbil':'31019',  'Abu Ghraib':'31020' ,
-                       'Sadah':'31021', 'Akashat':'31022' 
+                       'Al Waleed': '31015' , 'Balad':'31016',
+                       'Hatra':'31017','Al Ba\'aj' : '31018',
+                       'Bayji':'31019',  'Abu Ghraib':'31020' ,
+                       'Najaf':'31021', 'As Salaman':'31022' 
                        }
     
 def get_name(post_code):
