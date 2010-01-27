97c0bcd73f16cb5ff019ff55829ecc82473e9c1a
Diptanu Choudhury <admin.nitjece@gmail.com>
2010-01-27 10:30:59 +0200

    Added the Governorate model

---
+++ apps/charts/models.py
@@ -0,0 +1,28 @@
+from __future__ import division
+from math import ceil
+
+from django.contrib.gis.db import models
+from apps.poll.models import PollResponse
+from postcode_name_map import get_name
+
+class Governorates(models.Model):    
+    the_geom = models.PointField(srid=4326)
+    name = models.CharField(max_length=200)
+    objects = models.GeoManager()
+    
+    def num_responses(self):
+        self.post_code = get_name(self.name)
+        if self.post_code == "Not Found":
+            return None
+        return PollResponse.objects.filter(location = self.post_code).count()
+    
+    def total_responses(self):
+        self.poll_responses = PollResponse.objects.all()
+        return self.poll_responses.count()
+    
+    def style(self):
+        if self.num_responses():
+            print (self.num_responses() / self.total_responses())
+            return "s%d" % (int(ceil((self.num_responses() / self.total_responses()) * 100)))
+        else:
+            return None
