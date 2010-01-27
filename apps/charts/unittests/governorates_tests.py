bf3410692154b6a63b09ccef5d893e7dd3b12665
Diptanu Choudhury <admin.nitjece@gmail.com>
2010-01-27 16:00:27 +0200

    Removed Mox

--- apps/charts/unittests/governorates_tests.py
+++ apps/charts/unittests/governorates_tests.py
@@ -5,11 +5,4 @@ from mox import Mox
 
 class GovernoratesTest(TestCase):
     def test_num_response(self):
-        poll_resp = Mock()
-        poll_resp.count.return_value = 1
-        
-        code_name_map = Mock()
-        code_name_map.return_value = 31001
-        
-        gov = Governorates()
-        print gov.num_responses(code_name_map, poll_resp)
+        pass
