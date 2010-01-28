5549d177d245ee9a44a0b6bb216a83d894bfdefd
U-rotop\rowena <rowena@rotop.(none)>
2010-01-28 10:45:26 +0200

    Add i18n utility apps + unit tests

---
+++ apps/i18n/utils.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+import re
+
+DEFAULT_LANGUAGE = "en"
+
+def is_english(string):
+    string = string.strip()
+    if not string:
+        raise ValueError("Cannot infer language from empty string")
+    if string.isdigit():
+        raise ValueError("Cannot infer language from numbers")
+    try:
+        string.encode('ascii')
+        return True
+    except Exception, e:
+        return False
+
+def get_language_code(connection):
+    if connection.reporter:
+        if connection.reporter.language:
+            return connection.reporter.language
+    return DEFAULT_LANGUAGE 
+
+"""
+def get_language_from_code(language_code):
+    languages = Language.objects.all()
+    for language in languages:
+        if re.match(language.pattern.regex, language_code, re.IGNORECASE):
+            return language
+    if language_code == DEFAULT_LANGUAGE:
+        # so we don't infinite loop
+        return None
+    return get_language_from_code(DEFAULT_LANGUAGE)
+
+def get_translation(string, language_code):
+    try:
+        language = get_language_from_code(language_code)
+        if language:
+            return Translation.objects.get(language=language, original=string).translation
+    except Translation.DoesNotExist:
+        # hopefully the default passed in string will work
+        pass
+    return string
+"""
