071139d824964f1b7df41456c498b2ee926a9a3c
puneet goyal <puneet@puneet.(none)>
2010-01-31 08:29:24 +0200

    fixing tests

--- apps/poll/subtests/poll_response.py
+++ apps/poll/subtests/poll_response.py
@@ -9,28 +9,28 @@ class PollResponseTest(TestCase):
         self.poll_response = PollResponse(mobile_number = 1000)
 
     def test_parse_response(self):
-        response = self.poll_response.generate_response("ED 16 F 110001")
+        response = self.poll_response.generate_response("NE 16 F 110001")
 
         self.assertEquals(self.poll_response.age, '16')
         self.assertEquals(self.poll_response.gender,'F')
         self.assertEquals(self.poll_response.location,"110001")
-        self.assertEquals(response, "Thank you for voting. You selected Education.")
+        self.assertEquals(response, "Thank you for voting. You selected Never.")
         
     def test_location_is_optional(self):
-        response = self.poll_response.generate_response("ED 12 M")
+        response = self.poll_response.generate_response("AL 12 M")
 
         self.assertEquals(self.poll_response.age, '12')
         self.assertEquals(self.poll_response.gender,'M')
         self.assertEquals(self.poll_response.location, None)
-        self.assertEquals(response, "Thank you for voting. You selected Education.")
+        self.assertEquals(response, "Thank you for voting. You selected Always.")
 
     def test_extra_info_in_message_is_ignored(self):
-        response = self.poll_response.generate_response("ED 16 F 110001 Foo Bar")
+        response = self.poll_response.generate_response("RA 16 F 110001 Foo Bar")
 
         self.assertEquals(self.poll_response.age, '16')
         self.assertEquals(self.poll_response.gender,'F')
         self.assertEquals(self.poll_response.location,"110001")
-        self.assertEquals(response, "Thank you for voting. You selected Education.")
+        self.assertEquals(response, "Thank you for voting. You selected Rarely.")
 
     def test_incorrect_response_message_on_bad_parsing(self):
         self.assertRaises(ValueError, self.poll_response.generate_response, "ED M 12 110001")
