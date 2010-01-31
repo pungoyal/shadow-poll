67a2b2103be9d97684c4e474dea75bba67fe3753
U-rotop\rowena <rowena@rotop.(none)>
2010-01-31 17:11:04 +0200

    Restore app.py from echo app

--- apps/echo/app.py
+++ apps/echo/app.py
@@ -4,4 +4,14 @@ import re
 class App(rapidsms.app.App):
     prefix = re.compile(r'^echo\s+',re.I)
     def handle(self, message):
-        pass
+        self.debug("got message %s", message.text)
+        # for purposes of demonstration, have this 
+        # app respond to everything
+        response = "You said: %s" % message.text
+        self.debug("responding with %s", response)
+        message.respond(response)
+#        if self.prefix.search(message.text):
+#            response = self.prefix.sub("",message.text)
+#            self.debug("responding with %s", response)
+#            message.respond(response)
+        return True
