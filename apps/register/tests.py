3b92c96d51fb6a45d575f4e8e3bcacc6971fcb0d
rnjn <r@gogo.(none)>
2010-02-01 12:00:56 +0530

    moving register tests to different files

--- apps/register/tests.py
+++ apps/register/tests.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#imports for functional tests
 from django.test import TestCase
 from rapidsms import *
 from rapidsms.connection import *
@@ -10,6 +11,10 @@ from poll.models import Phone
 import poll.app as poll_app
 import register.app as register_app
 import reporters.app as reporter_app
+#model and app tests
+from subtests.models import *
+from subtests.app import *
+
 
 class TestRegisterScript (TestScript):
     apps = (poll_app.App, register_app.App, reporter_app.App)
@@ -49,59 +54,3 @@ class TestRegisterArabicScript (TestScript):
         1000 < %(error_msg)s
     """ % {"error_msg":arabic_error_message}
 
-class RegisterTest(TestCase):
-    def test_register_needs_keyword_at_the_start_of_the_message_base(self):
-        app = App(None)
-        result = app.handle(Message(text="regirrt poll 100 1001", connection=1000))
-        self.assertEquals(result, None)
-        result = app.handle(Message(text="register", connection=1000))
-        self.assertEquals(result, True)
-        result = app.handle(Message(text="register poll", connection=1000))
-        self.assertEquals(result, True)
-
-        result = app.handle(Message(text="register poll 100 1001", connection=Connection(backend = None, identity=1000)))
-        self.assertEquals(result, True)
-        
-class RegistrationTest(TestCase):
-    fixtures = ['registration']
-    
-    def setUp(self):
-        self.backend = PersistantBackend(slug="MockBackend")
-        self.backend.save()
-        self.reporter = Reporter(alias="ReporterName")
-        self.reporter.save()
-        self.connection = Connection(backend=self.backend, identity="1000")
-        self.pconnection = Phone(backend=self.backend, 
-                                 reporter=self.reporter, 
-                                 identity="1000")
-        self.pconnection.save()
-        self.reporter.connections.add(self.pconnection)
-        
-        self.reg = Registration()
-        message = Message(text='register poll 100 1001', connection=self.connection)
-        message.persistant_connection = self.pconnection
-        self.reg.parse(message)
-
-    
-    def test_parse(self):
-        self.assertEquals(self.reg.public_identifier, 'poll')
-        self.assertEquals(self.reg.governorate, '100')
-        self.assertEquals(self.reg.district, '1001')
-        self.assertEquals(self.reg.phone.identity, "1000")
-
-    def test_load_by_mobile_number(self):
-        query_result = Registration.objects.filter(phone__identity = '100')
-        self.assertEquals(query_result.count(), 0)
-
-        query_result = Registration.objects.filter(phone__identity = '1000')
-        self.assertEquals(query_result.count(), 1)
-        r = query_result.iterator().next()
-        self.assertNotEquals(r, None)
-
-    def test_to_string(self):
-        r = Registration()
-        r.public_identifier = "Poll"
-        r.governorate = "12"
-        r.district = "8"
-        r.phone = self.pconnection
-        self.assertEquals(str(r), "1000 Poll 12 8")
