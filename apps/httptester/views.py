a806f898c56da4314598e10b7ae21fc5b6189dad
U-rotop\rowena <rowena@rotop.(none)>
2010-02-08 16:52:57 -0500

    Revert "Using the text_msg in proxy"
    This reverts commit 3188e1587170c7e7d7067b9eafe7f0b8bcf78f3d.

--- apps/httptester/views.py
+++ apps/httptester/views.py
@@ -30,6 +30,6 @@ def proxy(req, number, message):
         # in Python 3000. We get 'quote' to work by encoding the string and
         # allowing it to be interpreted as ascii bytes (this still works 
         # for non-unicode strings)
-        urllib2.quote(text_msg))
+        urllib2.quote(message))
     f = urllib2.urlopen(url)
     return HttpResponse(f.read())
