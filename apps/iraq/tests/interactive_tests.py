fdfaf834804c1b8a4901c936529a8008ce64353c
U-rotop\rowena <rowena@rotop.(none)>
2010-02-10 14:15:12 -0500

    add unit tests for arabic interactive app

--- apps/iraq/tests/interactive_tests.py
+++ apps/iraq/tests/interactive_tests.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 from rapidsms.tests.scripted import TestScript
-from poll.app import App
-from poll.models import *
+from internationalization.app import App as i18n_app
 from reporters.app import App as reporter_app
 from tree.app import App as tree_app
 
 class TestApp(TestScript):
-    fixtures = ['poll_responses.json']
-    apps = (App, reporter_app, tree_app)
+    fixtures = ['dietary_preferences.json']
+    apps = (reporter_app, tree_app, i18n_app)
 
-    testBasicPoll = """
+    testBasicPoll = u"""
       10000 > food
       10000 < What is your favourite fruit?
       10000 > apple
@@ -21,14 +20,32 @@ class TestApp(TestScript):
       10000 < Thank you for sharing your dietary preferences.
     """
 
-    fails_testBasicPollArabic = """
-      10000 > arabicfood
-      10000 < arabicWhat is your favourite fruit?
-      10000 > arabicapple
-      10000 < arabicOf these vegetables, which is your favourite? Carrot, Tomato, Lettuce, Spinach
-      10000 > arabictomato
-      10000 < arabicWhat is your favourite drink? Please specify one of: lime, kiwi, ginger.
-      10000 > arabickiwi
-      10000 < arabicThank you for sharing your dietary preferences.
+    testBasicError = u"""
+      10000 > food
+      10000 < What is your favourite fruit?
+      10000 > apple
+      10000 < Of these vegetables, which is your favourite? Carrot, Tomato, Lettuce, Spinach
+      10000 > tree
+      10000 < Please specify one of: Carrot, Tomato, Lettuce, Spinach
+    """
+
+    testBasicPollArabic = u"""
+      10000 > طعام
+      10000 < ما هي الفاكهة المفضلة لديك؟
+      10000 > تفاح
+      10000 < ما هي الخضراوات المفضلة اليك ؟ جزر, طماطم, خس, سبانخ
+      10000 > طماطم
+      10000 < ما هو عصيرك المفضل؟ الرجاء إختيار : ليمون, كيوي, زنجبيل
+      10000 > كيوي
+      10000 < شكراً لك لمشاركتك معنا بالأغذية المفضلة لديك.
+    """
+
+    testBasicErrorArabic = u"""
+      10000 > طعام
+      10000 < ما هي الفاكهة المفضلة لديك؟
+      10000 > تفاح
+      10000 < ما هي الخضراوات المفضلة اليك ؟ جزر, طماطم, خس, سبانخ
+      10000 > فtreeطماطم
+      10000 < الرجاء إختيار : جزر, طماطم, خس, سبانخ
     """
 
