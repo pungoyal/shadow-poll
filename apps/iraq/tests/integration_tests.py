e01c11ac68c9c9e3339d686da7cc69d1f678085f
Diptanu Choudhury <admin.nitjece@gmail.com>
2010-02-12 16:49:20 +0530

    Added a test scenario in the testTreeAppFail

--- apps/iraq/tests/integration_tests.py
+++ apps/iraq/tests/integration_tests.py
@@ -34,6 +34,8 @@ class TestIntegration(TestScript):
         00919980131127 < The three things I need most are: (Prioritize your first, second, and third.)
         00919980131127 > c,d,y
         00919980131127 < Placeholder error for question number 2. Please re-send.
+        00919980131127 > z,d,a
+        00919980131127 < Placeholder error for question number 2. Please re-send.
         00919980131127 > c,d,a
         00919980131127 < Compared to my parents, my life in the future will be: a) Better; b) About the same; c) Worse; d) I don't know. Choose a,b,c or d.
         00919980131127 > p
