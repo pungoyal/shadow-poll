1d01c83c672f3cd75a2531ddca5233f890f79014
pungoyal <pungoyal@gmail.com>
2010-02-16 02:09:19 +0530

    changing the retrieval logic for maps. they collate data from tree.entries now

--- apps/charts/models.py
+++ apps/charts/models.py
@@ -2,7 +2,7 @@ from __future__ import division
 
 from math import ceil
 
-from apps.poll.models import PollResponse
+from apps.tree.models import Entry
 from django.contrib.gis.db import models
 
 from postcode_name_map import get_name
@@ -14,21 +14,18 @@ class Governorates(models.Model):
     objects = models.GeoManager()
     bounding_box = models.CharField(max_length=1000)
     
-    def num_responses(self, name_map = get_name, pollresponse_manager_filter = PollResponse.objects.filter):
-        self.post_code = name_map(self.name)
-        if self.post_code == "Not Found":
-            return None
-        return pollresponse_manager_filter(location = self.post_code).count()
+    def num_responses(self):
+        return Entry.objects.filter(governorate = self.id).count()
     
     def total_responses(self):
-        self.poll_responses = PollResponse.objects.all()
-        return self.poll_responses.count()
+        return Entry.objects.all().count()
     
     def style(self):
-        if self.num_responses():
-            return "s%d" % (int(ceil((self.num_responses() / self.total_responses()) * 100)))
+        number_of_responses = self.num_responses()
+        if number_of_responses:
+            return "s%d" % (int(ceil((number_of_responses / self.total_responses()) * 100)))
         else:
             return None
-        
+
     def exposed(self):
         return {'name': self.id}
--- apps/charts/unittests/governorates_tests.py
+++ apps/charts/unittests/governorates_tests.py
@@ -1,8 +1,24 @@
 from django.test import TestCase
 from charts.postcode_name_map import get_name
 from charts.models import Governorates
-from mox import Mox
+from apps.charts.models import Governorates
 
 class GovernoratesTest(TestCase):
+    fixtures = ['test_charts']
+    
     def test_num_response(self):
-        pass
+        states = Governorates.objects.all()
+
+        self.assertEquals(states[0].num_responses(), 1)
+        self.assertEquals(states[1].num_responses(), 1)
+        self.assertEquals(states[2].num_responses(), 2)
+        self.assertEquals(states[3].num_responses(), 0)
+        self.assertEquals(states[4].num_responses(), 0)
+
+    def test_style(self):
+        states = Governorates.objects.all()
+
+        self.assertEquals(states[0].style(), "s25")
+        self.assertEquals(states[2].style(), "s50")
+        self.assertEquals(states[9].style(), None)
+        self.assertEquals(states[1].style(), "s25")
