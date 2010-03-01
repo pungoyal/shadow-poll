8b7249e802b5f40f80d31bdcf154e5268c1032d9
U-rotop\rowena <rowena@rotop.(none)>
2010-03-01 10:45:24 +0530

    Renaming model 'Governorates' to 'governorate' (all django models should be named singularly - the admin ui looks weird otherwise). Added admin ui for charts.governorate.

--- apps/charts/models.py
+++ apps/charts/models.py
@@ -6,11 +6,14 @@ from poll.models import User, UserResponse
 
 MAX_SCALE_LENGTH_IN_STYLE = 18
 
-class Governorates(models.Model):    
+class Governorate(models.Model):    
     the_geom = models.PointField(srid=4326)
     name = models.CharField(max_length=200)
     objects = models.GeoManager()
     bounding_box = models.CharField(max_length=1000)
+    
+    def __unicode__(self):
+        return "%s" % (self.name)
 
     def num_responses(self):
         return len(UserResponse.objects.filter(user__governorate = self.id))
@@ -19,7 +22,7 @@ class Governorates(models.Model):
         return len(UserResponse.objects.all())
     
     def description(self):
-        return "Iraqi Governorates"
+        return "Iraqi Governorate"
     
     def style(self, question):
         most_voted_category = question.most_voted_category_by_governorate(self.id)
