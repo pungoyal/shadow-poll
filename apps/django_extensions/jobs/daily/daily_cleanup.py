0482a9a4ac7b26ff2692bfa790374ff94a4ca6b0
U-rotop\rowena <rowena@rotop.(none)>
2010-03-04 01:15:00 +0530

    Upgraded the django_extensions module

--- apps/django_extensions/jobs/daily/daily_cleanup.py
+++ apps/django_extensions/jobs/daily/daily_cleanup.py
@@ -5,11 +5,11 @@ Can be run as a cronjob to clean out old data from the database (only expired
 sessions at the moment).
 """
 
-from extensions.management.jobs import DailyJob
+from django_extensions.management.jobs import DailyJob
 
 class Job(DailyJob):
     help = "Django Daily Cleanup Job"
 
     def execute(self):
-	from django.bin.daily_cleanup import clean_up
-	clean_up()
+        from django.core import management
+        management.call_command("cleanup")
