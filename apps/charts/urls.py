47d9efbb39ec7d5929f9fa2641ab4cc849ab95dd
pungoyal <pungoyal@gmail.com>
2010-03-04 14:20:55 +0530

    fixing the top response on the graphs

--- apps/charts/urls.py
+++ apps/charts/urls.py
@@ -6,8 +6,10 @@ urlpatterns = patterns('charts',
                        (r'^messages/audio/(.*)$', 'views.play_audio'),
 
                        (r'^charts/$', 'views.home_page'),
+
                        (r'^charts/[qQ]uestion?(\d{1,2})$', 'views.show_iraq_by_question'),
                        (r'^charts/(\d{1,2})/[qQ]uestion?(\d{1,2})$', 'views.show_governorate_by_question'),
+
                        (r'^get_kml/[qQ]uestion(\d{1,2})$', 'views.get_kml_for_iraq'),
                        (r'^get_kml/(\d{1,2})/[qQ]uestion(\d{1,2})$', 'views.get_kml_for_governorate')
                        )
