4cd0b85ce5b2aff60c63c57597e88d5bec0b35ea
pungoyal <pungoyal@gmail.com>
2010-03-11 08:52:25 +0530

    verifying that the Entry has been marked as 'processed' after downloading the voice file.

--- apps/atom/tests/sync_atom_job.py
+++ apps/atom/tests/sync_atom_job.py
@@ -2,15 +2,25 @@ import os, os.path
 import shutil
 from unittest import TestCase
 
+from apps.atom.models import Entry
+
 from apps.atom.jobs.daily.sync_atom import SyncAtomJob
 
 class SyncAtomJobTest(TestCase):
     def test_run(self):
+        initial_count = len(Entry.objects.all())
+
         job = SyncAtomJob()
         voice_messages = job.sync_atom("http://iraqyouth.mepemepe.com/ivr/atom.xml")
         self.assertEquals(len(voice_messages), 1)
 
         voice_message = voice_messages[0]
+        self.file_name = voice_message.sound_file_name
         self.assertTrue(os.path.isfile(voice_message.sound_file_name))
 
-        os.remove(voice_message.sound_file_name)        
+        entries = Entry.objects.all()
+        self.assertEquals(len(entries), initial_count + 1)
+        self.assertTrue(entries[initial_count].processed)
+
+    def tearDown(self):
+        os.remove(self.file_name)        
