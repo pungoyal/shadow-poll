2e220600faa4b35a708d452e0a3b2931aa807e52
U-rotop\rowena <rowena@rotop.(none)>
2010-03-22 17:43:50 +0530

    Refactor map_mdg.js to use a layer factory

--- apps/charts/static/javascripts/map_mdg.js
+++ apps/charts/static/javascripts/map_mdg.js
@@ -1,53 +1,27 @@
 $(document).ready(function(){
 var geoserver_url = "http://iraqyouth.mepemepe.com/geoserver/ows";
-var poverty_overlay = new OpenLayers.Layer.WMS(
-                "poverty_overlay",
-                geoserver_url,
-                { 
-                   layers: 'unicef:iraq_mdgs',
-                   transparent: true,
-                   format: 'image/png',
-                   styles: 'mdgs_poverty'
-                },
-                {
-                   isBaseLayer: false,
-                   visibility: false
-                }
-);
-poverty_overlay.setOpacity(0.5);
-map.addLayer(poverty_overlay);
+function MDG_overlay_factory(name, style)
+{
+	var overlay = new OpenLayers.Layer.WMS(
+	                name,
+	                geoserver_url,
+	                { 
+	                   layers: 'unicef:iraq_mdgs',
+	                   transparent: true,
+	                   format: 'image/png',
+	                   styles: style
+	                },
+	                {
+	                   isBaseLayer: false,
+	                   visibility: false
+	                }
+	);
+	overlay.setOpacity(0.5);
+	return overlay;
+}
 
-var underweigh_overlay = new OpenLayers.Layer.WMS(
-                "underweigh_overlay",
-                geoserver_url,
-                { 
-                   layers: 'unicef:iraq_mdgs',
-                   transparent: true,
-                   format: 'image/png',
-                   styles: 'mdgs_underweight'
-                },
-                {
-                   isBaseLayer: false,
-                   visibility: false
-                }
-);
-underweigh_overlay.setOpacity(0.5);
-map.addLayer(underweigh_overlay);
+map.addLayer(MDG_overlay_factory("poverty_overlay", 'mdgs_poverty'));
+map.addLayer(MDG_overlay_factory("underweigh_overlay", 'mdgs_underweight'));
+map.addLayer(MDG_overlay_factory("under5mort_overlay", 'mdgs_under5mort'));
 
-var under5mort_overlay = new OpenLayers.Layer.WMS(
-                "under5mort_overlay",
-                geoserver_url,
-                { 
-                   layers: 'unicef:iraq_mdgs',
-                   transparent: true,
-                   format: 'image/png',
-                   styles: 'mdgs_under5mort'
-                },
-                {
-                   isBaseLayer: false,
-                   visibility: false
-                }
-);
-under5mort_overlay.setOpacity(0.5);
-map.addLayer(under5mort_overlay);
 });
