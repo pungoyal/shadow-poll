f0b53715a669bab24983ee19511bbe9820202ef0
pungoyal <pungoyal@gmail.com>
2010-03-24 16:24:56 +0530

    fixing layer switching

--- apps/charts/static/javascripts/map_results.js
+++ apps/charts/static/javascripts/map_results.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     /**
      * BUBBLES
@@ -9,86 +9,87 @@ $(document).ready(function(){
     var governorate_id = $("#governorate-id").html();
     var gender = $("#gender-filter").html();
     var age = $("#age").html();
+
     function construct_kml_url(governorate_id, question_id, gender, age_group)
     {
-        var kml_url = "/get_kml/question" + question_id ;
+        var kml_url = "/get_kml/question" + question_id;
         if (governorate_id != null && governorate_id != '')
         {
-            kml_url = kml_url + "/governorate" + governorate_id ;
+            kml_url = kml_url + "/governorate" + governorate_id;
         }
-				if(gender != null && gender != '')
-				{
-						kml_url = kml_url + "?gender=" + gender;
-				}
-				if(age != null && age != '')
-				{
-						kml_url = kml_url + "&age=" + age;
-				}
+        if (gender != null && gender != '')
+        {
+            kml_url = kml_url + "?gender=" + gender;
+        }
+        if (age != null && age != '')
+        {
+            kml_url = kml_url + "&age=" + age;
+        }
         return kml_url;
     }
 
     kml_url = construct_kml_url(governorate_id, question_id, gender, age);
     var bubbles = new OpenLayers.Layer.Vector(
-        "poll_responses", 
-        {
-            projection: map.displayProjection,
-            strategies: [new OpenLayers.Strategy.Fixed()],
-            styleMap: new OpenLayers.StyleMap(
-              OpenLayers.Util.applyDefaults(
-                  {
-                    fillColor: "#93ce54", 
-                    fillOpacity: 1, 
+            "poll_responses",
+    {
+        projection: map.displayProjection,
+        strategies: [new OpenLayers.Strategy.Fixed()],
+        styleMap: new OpenLayers.StyleMap(
+                OpenLayers.Util.applyDefaults(
+                {
+                    fillColor: "#93ce54",
+                    fillOpacity: 1,
                     strokeColor: "#4c8014",
                     pointRadius: 4
-                  },
-                  OpenLayers.Feature.Vector.style["default"]
-                )
-            ),
-            protocol: new OpenLayers.Protocol.HTTP
-            ({
-                url: kml_url,
-                format: new OpenLayers.Format.KML
+                },
+                        OpenLayers.Feature.Vector.style["default"]
+                        )
+                ),
+        protocol: new OpenLayers.Protocol.HTTP
                 ({
-                    extractStyles: true,
-                    extractAttributes: true
+                    url: kml_url,
+                    format: new OpenLayers.Format.KML
+                            ({
+                                extractStyles: true,
+                                extractAttributes: true
+                            })
                 })
-            })
-        });
+    });
     bubbles.setOpacity(0.9);
     map.addLayer(bubbles);
-    
+
     /**
      * CONTROLS
      */
-    
+
     if (governorate_id == '')
     {
-	    function onFeatureSelect(event) {
-	        var feature = event.feature;
-	        governorate_id = feature.attributes['id'];
-	        window.document.location = '/charts/' + 'question' + question_id + '/governorate' +governorate_id;
+        function onFeatureSelect(event) {
+            var feature = event.feature;
+            governorate_id = feature.attributes['id'];
+            window.document.location = '/charts/' + 'question' + question_id + '/governorate' + governorate_id;
 
-	    }
-	    
-	    bubbles.events.on({
-	        "featureselected": onFeatureSelect
-	    });
-    
+        }
+
+        bubbles.events.on({
+            "featureselected": onFeatureSelect
+        });
+
         select = new OpenLayers.Control.SelectFeature([bubbles]);
         map.addControl(select);
         select.activate();
     }
-    
+
     // This makes sure we zoom to the appropriate country/governorate level
     var bbox = $("#bbox").html();
     var governorate_zoom = $("#governorate-zoom").html();
     if (bbox != '')
-    {	
+    {
         wkt = new OpenLayers.Format.WKT();
         vector = wkt.read(bbox);
         bounds = vector.geometry.getBounds().transform(
-        			new OpenLayers.Projection('EPSG:4326'), 
-        			new OpenLayers.Projection('EPSG:900913'));
+                new OpenLayers.Projection('EPSG:4326'),
+                new OpenLayers.Projection('EPSG:900913'));
         map.zoomToExtent(bounds);
         if (governorate_zoom != '')
         {
