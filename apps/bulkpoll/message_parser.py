39c43fa5d55f12dd8e508f5b35df08c5c0c8416d
pungoyal <pungoyal@gmail.com>
2010-04-02 12:07:21 +0530

    fixing bulk response for wrong answers

--- apps/bulkpoll/message_parser.py
+++ apps/bulkpoll/message_parser.py
@@ -6,28 +6,39 @@ from poll.models import Questionnaire
 class BulkMessageProcessor(object):
     def __init__(self, message):
         self.message = message
-        self.answer_list = []
         self.questionnaire = None
         
     def parse_and_create_user(self, connection, message):
-        message_arr = self.message.split(" ")
-        self.user = User(connection = connection, governorate = connection.governorate, district = connection.district, active = True)
-        self.answer_list.append(message_arr[3])
-        self.answer_list.append(" ".join(message_arr[4:7]))
-        self.answer_list.append(message_arr[7])
-        self.answer_list.reverse()
-        return self.answer_list
+        answers = []
 
-    def save_user_and_responses(self, message_arr):
+        message_parts = self.message.split(" ")
+        self.user = User(connection = connection, governorate = connection.governorate, district = connection.district, active = True)
+        answers.append(message_parts[3])
+        answers.append(" ".join(message_parts[4:7]))
+        answers.append(message_parts[7])
+        
+        return answers
+
+    def save_user_and_responses(self, answers):
         self.user.save()
         self._set_default_questionnaire()
 
-        question_list = Question.objects.filter(questionnaire = self.questionnaire).order_by('id')
-        
-        for (counter, question) in enumerate(question_list):
-            choices = question.matching_choices(self.answer_list[counter])
-            for ch in choices:
-                UserResponse(user = self.user, question = question, choice = ch).save()
+        questions = Question.objects.filter(questionnaire = self.questionnaire).order_by('id')
+
+        matching_choices = []
+
+        for (counter, question) in enumerate(questions):
+            choices_for_one_question = question.matching_choices(answers[counter])
+
+            matching_choices.append(choices_for_one_question)
+
+            if len(choices_for_one_question) < 1:
+                return "bulk_response_error"
+
+        for (counter, question) in enumerate(questions):
+            for choice in matching_choices[counter]:
+                UserResponse(user = self.user, question = question, choice = choice).save()
+
         return "thanks"
     
     def _set_default_questionnaire(self):
